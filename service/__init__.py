7e546200cd971532edae89a1c720ddffa9a7c337
reeceparry <reecerox2@gmail.com>
2025-09-22 10:57:04 -0400

    commit changes #3

--- service/__init__.py
+++ service/__init__.py
@@ -1,16 +1,16 @@
 """
 Service Package
 """
+
 from flask import Flask
+from service.common import log_handlers
+from service import routes  # noqa: F401  # needed for route registration
 
 app = Flask(__name__)
 
-# This must be imported after the Flask app is created
-from service import routes               # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  # pylint: disable=wrong-import-position
-
+# Initialize logging
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(" SERVICE RUNNING ".center(70, "*"))
 app.logger.info(70 * "*")
